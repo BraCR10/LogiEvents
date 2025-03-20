fc11cd229165120edef442b483716cef51e2b48a
BraCR10 <brianramirez01arias@gmail.com>
2025-03-20 13:54:34 -0600

    Adding auth styles sheet

--- client/app/auth/login.tsx
+++ client/app/auth/login.tsx
@@ -1,16 +1,42 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
 import React from "react";
+
 export default function HomeScreen() {
   const router = useRouter();
 
   const goHome = () => {
       router.push("/home"); 
   };
+
+  const goRegister = () => {
+        router.push("/auth/register");
+    }
+  
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>Please login!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Please login!</Text>
       <Button title="Login" onPress={goHome} />
+      <View style={styles.registerBtn}>
+        <Button title="Register" onPress={goRegister} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 24, 
+    marginBottom: 20
+  },
+
+  registerBtn : {
+    marginTop: 20,
+    alignItems: "center"
+  }
+});
