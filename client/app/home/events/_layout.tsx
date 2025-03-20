641505b639e7cba1d151e009dd31a3e4db7d6cce
BraCR10 <brianramirez01arias@gmail.com>
2025-03-20 14:42:21 -0600

    Deleting layout event

--- client/app/home/events/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-import React from "react";
-
-export default function RootLayout() {
-  return(<Stack>
-    <Stack.Screen name="index" options={{headerShown: false,title: 'events'}} />
-    <Stack.Screen name="[eventId]" options={{ headerShown: false,title: 'Event information' }} />
-  </Stack>);
-}
