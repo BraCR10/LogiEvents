7be30252c096eb6686609782e872ea147a61cb2c
BraCR10 <brianramirez01arias@gmail.com>
2025-03-26 07:24:50 -0600

    Adding home page
    -TODO: Integrate API

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, Button, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
 import React from "react";
+import Navbar from "@/components/Navbar";
 
 export default function HomeScreen() {
   const router = useRouter();
@@ -10,14 +11,24 @@ export default function HomeScreen() {
   };
   
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to LogiEvents</Text>
-      <Button title="Try login" onPress={goHome} />
+    <View style={styles.main}>
+      <Navbar isLogged={false} onEventClick={() => {}} />
+      <View style={styles.container}>
+        
+        <Text style={styles.title}>Welcome to LogiEvents</Text>
+        <Button title="Try login" onPress={goHome} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  main : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
   container: {
     flex: 1, 
     justifyContent: "center", 
